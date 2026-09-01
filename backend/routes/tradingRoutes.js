988a56638813cfda02011e3d2baa69cd691f3493
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-09-01 15:45:48 +0530

    by malik

--- backend/routes/tradingRoutes.js
+++ backend/routes/tradingRoutes.js
@@ -7,13 +7,16 @@ const {
   getMyActiveTrade,
 } = require("../controllers/tradingController");
 
+const { protect, } = require("../middleware/authMiddleware.js");
+
+
 const router = express.Router();
 
-router.post("/round/create", createRound);
-router.get("/round/current", getCurrentRound);
-router.post("/round/complete", completeRound);
+router.post("/round/create", protect, createRound);
+router.get("/round/current", protect, getCurrentRound);
+router.post("/round/complete", protect, completeRound);
 
-router.post("/trade/place", placeTrade);
-router.get("/trade/active", getMyActiveTrade);
+router.post("/trade/place", protect, placeTrade);
+router.get("/trade/active", protect, getMyActiveTrade);
 
 module.exports = router;
